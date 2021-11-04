02782acc08a6bb058f357414cc7e0d6d76ca743e
andreahowes <andrea.howes01@gmail.com>
2021-11-03 21:18:49 -0400

    added data processor and data bean

---
+++ src/main/java/edu/gmu/swe642/DataBean.java
@@ -0,0 +1,23 @@
+package edu.gmu.swe642;
+
+public class DataBean {
+    private Double mean;
+    private Double standardDev;
+
+	public Double getMean() {
+		return mean;
+	}
+
+	public void setMean(Double mean) {
+		this.mean = mean;
+	}
+
+    public Double getStandardDev() {
+        return standardDev;
+    }
+
+    public void setStandardDev(Double standardDev) {
+        this.standardDev = standardDev;
+    }
+}
+
